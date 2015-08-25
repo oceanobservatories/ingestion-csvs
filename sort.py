b2e5a2169ff9bedea6c14aa2120d431f1261dfc3
Shariq <shariq.ansari@rutgers.edu>
2015-08-25 11:10:06 -0400

    Repository Management
    Move Platform Parameters Checklist file to its own directory.
    Add sorting script.

---
+++ sort.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+from glob import glob
+
+# Sort Calibration Sheets
+for f in [x for x in glob("*.xlsx") if x.startswith("Omaha")]:
+    platform_name = f.split("_")[3]
+    if not os.path.exists("./%s" % platform_name):
+        shutil.os.mkdir(platform_name)
+    shutil.move(f, platform_name)
+
+# Sort Ingestion Parameter Sheets
+for f in [x for x in glob("*.csv")]:
+    platform_name = f.split("_")[0]
+    if not os.path.exists("./%s" % platform_name):
+        shutil.os.mkdir(platform_name)
+    shutil.move(f, platform_name)
+
+# Sort Ingestion Parameter Template Sheets
+for f in [x for x in glob("*.xlsx")]:
+    platform_name = f.split("_")[0]
+    if not os.path.exists("./%s" % platform_name):
+        shutil.os.mkdir(platform_name)
+    shutil.move(f, platform_name)
+
