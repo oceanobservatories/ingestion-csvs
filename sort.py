b46df5d2912be377ba6dbbcee95c9af1636ac401
Shariq Ansari <shariqan@oit.rutgers.edu>
2015-08-25 16:23:15 -0400

    Update sort.py to overwrite files. Update calibration sheets for various platforms as per Redmine #8310.

--- sort.py
+++ sort.py
@@ -7,19 +7,22 @@ for f in [x for x in glob("*.xlsx") if x.startswith("Omaha")]:
     platform_name = f.split("_")[3]
     if not os.path.exists("./%s" % platform_name):
         shutil.os.mkdir(platform_name)
-    shutil.move(f, platform_name)
+    shutil.copy(f, platform_name)
+    os.remove(f)
 
 # Sort Ingestion Parameter Sheets
 for f in [x for x in glob("*.csv")]:
     platform_name = f.split("_")[0]
     if not os.path.exists("./%s" % platform_name):
         shutil.os.mkdir(platform_name)
-    shutil.move(f, platform_name)
+    shutil.copy(f, platform_name)
+    os.remove(f)
 
 # Sort Ingestion Parameter Template Sheets
 for f in [x for x in glob("*.xlsx")]:
     platform_name = f.split("_")[0]
     if not os.path.exists("./%s" % platform_name):
         shutil.os.mkdir(platform_name)
-    shutil.move(f, platform_name)
+    shutil.copy(f, platform_name)
+    os.remove(f)
 
