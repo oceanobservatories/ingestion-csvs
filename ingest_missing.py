953307806c45f02dff6ee3b9b50fb8c09fdcd426
Phil Tran <philtrann@gmail.com>
2018-03-28 12:08:15 -0700

    Simplified the script and allowed hours to be ingested as well.

--- ingest_missing.py
+++ ingest_missing.py
@@ -1,12 +1,15 @@
 """
-Usage: ingest_missing.py <event_url> <particle_url>
+Usage: ingest_missing.py <event_url> <particle_url> <server>
 
 This script will grab dates missing from data ingestion and then it will use the playback command to ingest them.
 Provide the event_url and particle_url. 
 
+Example: python ingest_missing.py qpid://guest/guest@uframe-test?queue=Ingest.instrument_events qpid://guest/guest@uframe-test?queue=Ingest.instrument_particles uframe-test
+
 Arguments:
     event_url    
     particle_url
+    server          This is where the reference designator information is stored.
 
 Options:
     -h --help
@@ -19,43 +22,42 @@ import sys
 import time
 import requests
 import re
+import glob
 import list_missing_dates
 from subprocess import call
 from datetime import datetime, timedelta
 
+g_cabled_drivers_raw = []
 
-"""
-Get the driver from the cabled_drivers_list.txt file
-"""
-def get_driver(refdes):
+def request_cabled_raw():
     drivers_conf_git_link = "https://raw.githubusercontent.com/ooi-integration/ingestion-csvs/master/cabled_drivers_list.txt"
-    driver_conf_raw = str(requests.get(drivers_conf_git_link).text)
+    return str(requests.get(drivers_conf_git_link).text).split('\n')
+    
+def get_driver(refdes):
+    """
+    Get the driver from the cabled_drivers_list.txt file
+    """
+    global g_cabled_drivers_raw
 
-    driver_conf_raw = driver_conf_raw.split('\n')
-
-    for line in driver_conf_raw:
+    for line in g_cabled_drivers_raw:
         if refdes in line:
             return line.split()[1]
 
     return None
 
-"""
-Get the reader type from the cabled_drivers_list.txt file
-"""
 def get_reader_type(refdes):
-    drivers_conf_git_link = "https://raw.githubusercontent.com/ooi-integration/ingestion-csvs/master/cabled_drivers_list.txt"
-    driver_conf_raw = str(requests.get(drivers_conf_git_link).text)
-
-    driver_conf_raw = driver_conf_raw.split('\n')
+    """
+    Get the reader type from the cabled_drivers_list.txt file
+    """
+    global g_cabled_drivers_raw
     first_pass = False
     reader = []
 
-    for line in driver_conf_raw:
+    for line in g_cabled_drivers_raw:
         if refdes in line:
             first_pass = True
             reader.append(line.split()[2])
-        elif first_pass:
-            
+        elif first_pass: 
             if line.split()[0] != 'datalog' and \
                 line.split()[0] != 'chunky' and \
                 line.split()[0] != 'ascii':
@@ -63,30 +65,25 @@ def get_reader_type(refdes):
                 break
             else:
                 reader.append(line.split()[0])
-    
     return reader
 
-"""
+def is_cabled(refdes):
+    """
     Only get cabled assemblies.
     Table provided by the cabled_drivers_list.txt file.
-""" 
-def get_cabled_assembly(refdes):
-    assembly_set = {'lj01c', 'lv01a', 'lv03a', 'mj01c', 'mj03c', 
-                    'mj03f', 'pc03a', 'pd03a', 'sf03a', 'lj01a', 
-                    'lj01d', 'lv01b', 'mj01a', 'mj03a', 'mj03d', 
-                    'pc01a', 'pd01a', 'sf01a', 'lj01b', 'lj03a', 
-                    'lv01c', 'mj01b', 'mj03b', 'mj03e', 'pc01b', 
-                    'pd01b', 'sf01b'}
-    assembly = refdes.split('-')[1]
-    if assembly in assembly_set:
-        return assembly
-    else:
-        return None
+    """ 
+    global g_cabled_drivers_raw
 
-"""
-    Obtain a list of the missing dates.
-"""
+    for line in g_cabled_drivers_raw:
+        if refdes in line:
+            return True
+    else:
+        return False
+
 def date_list(firstDate, secondDate):
+    """
+    Obtain a list of the missing dates.
+    """
     startDate = 0
     endDate = 0
     missing_dates_list = []
@@ -99,226 +96,60 @@ def date_list(firstDate, secondDate):
         endDate = firstDate
 
     # Working with the dates, getting the times between the start date and end date
-    startDate = datetime.strptime(startDate, '%Y-%m-%d').date()
-    endDate = datetime.strptime(endDate, '%Y-%m-%d').date()
-    delta = endDate - startDate
-
-    # Get all dates in between startDate and endDate (inclusive)
-    # to remove startDate and endDate, range(1, delta.days)
-    for i in range(delta.days + 1):
-        tempDate = startDate + timedelta(days = i)
-        tempDate = tempDate.strftime('%Y-%m-%d').replace('-','')
-        missing_dates_list.append(tempDate)
-
-    return missing_dates_list
-
-"""
-    This function will organize the missing dates into nested dictionaries.
-    This will make it easier to put together the directory glob with the missing dates.
-    Format of raw date is YYYYMMDD
-"""
-def organize_dates(missing_dates):
-    year = ''
-    years_dict = {}
-    years = []
-    month = ''
-    months_dict = {}
-    months = []
-    day = ''
-    days_dict = {}
-    days = []
-
-    # This will organize the dates into nested dictionaries
-    """
-        {YYYY :
-                {MM :
-                    {D :
-                        [D, D, D ,D]
-                    }
-                }
-                {MM :
-                    {D :
-                        [D, D, D ,D]
-                    }
-                }
-        }
-    """
-    for i, date in enumerate(missing_dates):
-        temp_year = date[0] + date[1] + date[2] + date[3]
-        temp_month = date[4] + date[5]
-        # Just get first digit of the day
-        temp_day = date[6]
-
-        if not year:
-            year = temp_year
-            month = temp_month
-            day = temp_day
-
-        elif year != temp_year or i >= len(missing_dates) - 1:
-            days.append(date[7])
-            days_dict[day] = days
-            months_dict[month] = days_dict
-            years_dict[year] = months_dict
-
-            # The year has now changed, then so does the month and the day
-            del days_dict
-            days_dict = {}
-            day = temp_day
-            del days
-            days = []
-            year = temp_year
-            del years
-            years = []
-            del months_dict
-            months_dict = {}
-            del months
-            months = []
-            month = temp_month
-            day = temp_day
-            
-        
-        if year == temp_year:
-            if month == temp_month:
-                if day == temp_day:
-                    days.append(date[7])
-                elif day != temp_day:
-                    # The month has not changed in this case, so the very last day of the month
-                    # will not change. In the clase where the month does change, so does the day.
-                    # That day change will be made in the following elif statement
-                    days_dict[day] = days
-                    day = temp_day
-                    del days
-                    days = []
-                    days.append(date[7])
-
-            elif month != temp_month:
-                # In the case where the month has changed. So day, also needs to change.
-                if day != temp_day:
-                    days_dict[day] = days
-                    day = temp_day
-                    del days
-                    days = []
-                    days.append(date[7])
-                
-                months_dict[month] = days_dict
-
-                month = temp_month
-                del days_dict
-                days_dict = {}
-                
-    return years_dict
-
-"""
-    Get the dates in the correct format.
-    Future considerations would be to shrink the size of this even further.
-"""
-def glob_dates(organize_dates):
-    date_glob = '{'
+    startDate = datetime.strptime(startDate, '%Y-%m-%d %H:%M:%S')
+    endDate = datetime.strptime(endDate, '%Y-%m-%d %H:%M:%S')
     
-    for i, (year, months_dict) in enumerate(organize_dates.iteritems()):
-        date_glob += year + '{'
-        for j, (month, days_dict) in enumerate(months_dict.iteritems()):
-            date_glob += month + '{'
-            first_day = True
+    date_times = [startDate.strftime('%Y%m%dT%H')]
+    date_time = startDate
+    
+    while date_time < endDate:
+        date_time += timedelta(hours=1)
+        date_times.append(date_time.strftime('%Y%m%dT%H'))
 
-            for k, (day, ones_list) in enumerate(days_dict.iteritems()):
-                if first_day:
-                    date_glob += day + '['
-                    first_day = False
-                    
-                for ones in ones_list:
-                    date_glob += ones
-                    first_day = True
-                if k < len(days_dict) - 1:
-                    date_glob += '],'
-                else:
-                    date_glob +=']'
+    return date_times
 
-            if j < len(months_dict) - 1:
-                date_glob += '},'
-            else:
-                date_glob += '}'
-
-        if i < len(organize_dates) - 1:
-            date_glob += '},'
-        else:
-            date_glob += '}'
-    date_glob += '}'
-    return date_glob
-
-"""
-    This is the main function to get the dates into their specified
-    directory glob format. This will be used for the playback command.
-"""
-def get_dir_glob(missing_dates_dict):
-    base_dir = '/rsn_cabled/rsn_data/DVT_Data/'
-    directory = ''
-    instrument = ''
-    missing_dates = []
-    playback_dict = {}
-
-    for key, date_tup_list in missing_dates_dict.iteritems():
-        firstDate = ''
-        secondDate = ''
-        
-        refdes = key
-        assembly = get_cabled_assembly(refdes.lower())
-        instrument = refdes.split('-')[3]
-
-        directory = base_dir + assembly + '/' + instrument + '*'
-
-        for first, second in date_tup_list:
-            firstDate = first
-            secondDate = second
-            missing_dates += date_list(firstDate, secondDate)
-
-        organized_dates = organize_dates(missing_dates)
-        dates_glob = glob_dates(organized_dates)
-        directory_glob = directory + dates_glob + '*'
-        playback_dict[refdes] = directory_glob
-
-    return playback_dict
-
-"""
+def playback(refdes, event_url, particle_url, missing_dates):
+    """
     Put together the directory glob and other options for the playback command.
     This will then playback cabled data.
-"""
-def playback(directory, refdes, event_url, particle_url):
+    """
+    main_directory = '/rsn_cabled/rsn_data/DVT_Data'
     driver = get_driver(refdes)
     reader_list = get_reader_type(refdes)
-
-    playback_command = 'playback ' + driver + ' ' + refdes + ' ' + event_url + particle_url + directory
+    node = refdes.split('-')[1].lower()
+    instrument = refdes.split('-')[3]
 
     for reader in reader_list:
-        playback_command = 'playback ' + reader + ' ' + driver + ' ' + refdes + ' ' + event_url + particle_url + directory
-        print playback_command + '\n'
-        call(playback_command, shell=True)
+        for date in missing_dates:
+            directory = '/'.join([main_directory, node, instrument])
+            directory += '*'.join(['', date, ''])
+
+            # Check to see if this particular file exists before executing callback
+            if glob.glob(directory):
+                playback_command = ' '.join(['playback', reader, driver, refdes, event_url, particle_url, directory])
+                call(playback_command, shell=True)
 
 def main(args):
-
-    if args['<event_url>'] and args['<particle_url>']:
-        event_url = args['<event_url>'] + ' '
-        particle_url = args['<particle_url>'] + ' '
-        print "Getting list of Reference Designators"
-        refdes_list = list_missing_dates.get_refdes_list()
+    if args['<event_url>'] and args['<particle_url>'] and args['<server>']:
+        global g_cabled_drivers_raw
+        g_cabled_drivers_raw = request_cabled_raw()
+        event_url = args['<event_url>']
+        particle_url = args['<particle_url>']
+        server = args['<server>']
+        refdes_list = list_missing_dates.get_refdes_list(server)
         missing_data_dict = {}
         
-        print "Assembling each date to the specific reference designator"
         for refdes in refdes_list:
-            assembly = get_cabled_assembly(refdes.lower())
-            if assembly:
-                missing_data_list = list_missing_dates.get_missing_data_list(refdes) # returns tuple
+            if is_cabled(refdes):
+                missing_data_list = list_missing_dates.get_missing_data_list(refdes, server) # returns tuple
                 if missing_data_list:
                     missing_data_dict[refdes] = missing_data_list
-            
         
-        playback_dict = get_dir_glob(missing_data_dict)
-
-        for refdes, directory_glob in playback_dict.iteritems():
-            playback(directory_glob, refdes, event_url, particle_url)
-    
+        for refdes, missing_dates in missing_data_dict.iteritems():
+            for date in missing_dates:
+                dates = date_list(date[0], date[1])
+                playback(refdes, event_url, particle_url, dates)
 
 if __name__ == "__main__":
     args = docopt(__doc__)
-    print(args)
     main(args)
--- list_missing_dates.py
+++ list_missing_dates.py
@@ -1,32 +1,33 @@
 import sys
 import requests, json
 
-def get_refdes_list():
-    http = 'http://uframe-3-test:12576/sensor/inv/'
+
+def get_refdes_list(server):
+    """
+    Get list of reference designators that are available on the server
+    """
+    http = ''.join(['http://', server, ':12576/sensor/inv/'])
     r = requests.get(http)
-    refdes_json = r.json()
-    site_list = []
     refdes_list = []
     
-    for item in refdes_json:
-        site = item
-        site_http = http + item + '/'
+    for site in r.json():
+        site_http = http + site + '/'
         site_node = requests.get(site_http)
         
-        for item1 in site_node.json():
-            node = item1
-            node_http = site_http + item1 + '/'
+        for node in site_node.json():
+            node_http = site_http + node + '/'
             site_node_inst = requests.get(node_http)
             
-            for item2 in site_node_inst.json():
-                inst = item2
+            for inst in site_node_inst.json():
                 refdes_list.append(site + '-' + node + '-' + inst)
-                print site + '-' + node + '-' + inst
     return refdes_list
 
 
-def get_missing_data_list(refdes):
-    http = "http://uframe-3-test.intra.oceanobservatories.org:9000/available/" + refdes
+def get_missing_data_list(refdes, server):
+    """ 
+    Get list of missing dates
+    """
+    http = ''.join(['http://', server, '.intra.oceanobservatories.org:9000/available/', refdes])
     http = http.replace('\n', '')
     r = requests.get(http)
     data = r.json()
@@ -35,13 +36,11 @@ def get_missing_data_list(refdes):
     if len(data['availability']) > 1:
         for i in data['availability'][1]['data']:
             if i[1] == "Missing":
-                startDate = i[0].split()[0]
-                endDate = i[2].split()[0]
+                startDate = i[0]
+                endDate = i[2]
                 missing_data_list.append((startDate, endDate))
                 
         if len(missing_data_list) < 1:
             return None
         else:
             return missing_data_list
-    
-    return None
